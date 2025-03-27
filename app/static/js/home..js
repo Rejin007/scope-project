c12080221dfd0826ab88f1ce37350c51b471e1e4
Rejin <rejinrjr144@gmail.com>
2025-03-28 00:02:23 +0530

    upload today's work

---
+++ app/static/js/home..js
@@ -0,0 +1,28 @@
+
+
+// var array = [
+//     '<img class="home1" src="{% static 'assets/AI&ML-course-scopeindia.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/data-science&ai-course-scopeindia.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/Devops-training-scopeindia.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/digital-marketing-course-scopeindia.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-aws-certification-course-(1).jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-azure-certification-course.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-digital-marketing-course.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-dotnet-core-full-stack-course.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-flutter-fullstack-course.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-java-full-stack-course (1).jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-mean-mern-stack-course.jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-network-server-administration-course (1).jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-php-full-stack-course (1).jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-python-full-stack-course (1).jpg' %}" alt="">',
+//     '<img class="home1" src="{% static 'assets/scope-india-software-testing-manual-automation-course.jpg' %}" alt="">'
+// ];
+// var drag=0;
+// function fun(){
+//     var img = document.getElementById("image_container");
+//     drag ++;
+//     if(drag >= array.length){
+//         drag = 0;
+//     }
+//     img.innerHTML=array[drag]
+// }
